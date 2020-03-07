91c7f5da046a21aab2bda3df628e4de6fedb1a95
murffious <titan.murff@gmail.com>
2020-03-06 23:21:39 -0700

    pushing template code2

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import indexRoutes from "routes/index.jsx";
+
+import "assets/scss/material-dashboard-pro-react.css?v=1.3.0";
+
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
